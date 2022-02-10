0809cdecaee40ef8a00040c3cbb64a11ab4304e8
Lucca Rendall <luccarendall1@gmail.com>
2022-02-10 03:35:41 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const feedbacks = () => 'Eita pessoa boa!'; 
+const createStudent = (nome) => ({ name: nome, feedback: feedbacks });  
 
 module.exports = createStudent;
